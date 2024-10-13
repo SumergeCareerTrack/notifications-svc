d0218d2629f89769b49cbc6cf601b56ab6025026
Andysm21 <102580958+Andysm21@users.noreply.github.com>
2024-10-13 10:02:22 +0300

    Initial Commit

---
+++ src/main/java/com/sumerge/careertrack/notifications_svc/NotificationsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.notifications_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsSvcApplication.class, args);
+	}
+
+}
